811dd837432e5a6622ada906611fa73bd52800da
DonMako <80451849+DonMako@users.noreply.github.com>
2025-02-18 23:54:50 +0100

    Mettre en place la fonction d'appel de la police Dys

--- src/js/ChangePolice.js
+++ src/js/ChangePolice.js
@@ -1,31 +1,12 @@
-const changeSerif = () => {
+const changeToOpenDyslexic = () => {
     const element = document.getElementById('pdf');
     if (element != null) {
-        element.style.fontFamily = "serif";
-    }
-}
-
-const changeMonospace = () => {
-    const element = document.getElementById('pdf');
-    if (element != null) {
-        element.style.fontFamily = "monospace";
-    }
-}
-
-const changeCursive = () => {
-    const element = document.getElementById('pdf');
-    if (element != null) {
-        element.style.fontFamily = "cursive";
+        element.toggleCass("active");
     }
 }
 
 const ChangePolice = () => {
-    return <div id="police">
-        <p id="choix">Choix de la police du texte</p>
-        <button aria-label="Mettre la police Serif" onClick={changeSerif} style={{'font-family':'serif'}}>A</button>
-        <button aria-label="Mettre la police Monospace" onClick={changeMonospace} style={{'font-family':'monospace'}}>A</button>
-        <button aria-label="Mettre la police Cursive" onClick={changeCursive} style={{'font-family':'cursive'}}>A</button>
-    </div>
+    return <button aria-label="Mettre la police Cursive" onClick={changeToOpenDyslexic} style={{'font-family':'OpenDyslexic'}}>A</button>
 }
 
 export default ChangePolice;
