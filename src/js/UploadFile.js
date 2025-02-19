89c19f2f9f5425579802a4c3b36b3e3bf73ac6f5
DonMako <80451849+DonMako@users.noreply.github.com>
2025-02-19 22:25:04 +0100

    TEST : UploadFile.js simplifié + permet convertion images -> texte

--- src/js/UploadFile.js
+++ src/js/UploadFile.js
@@ -1,67 +1,33 @@
 import React, { forwardRef } from "react";
-import { Button, Grid } from "@mui/material";
 
-const getRandomInt = (max = 1000000000) => {
-    return Math.floor(Math.random() * Math.floor(max));
-};
+const Optiic = require('optiic');
+const optiic = new Optiic();
+
+const convertImage = (image) => {
+  optiic.process({image: image}).then(result => result);
+}
 
 const UploadFile = forwardRef(
   (
     {
-      sx,
-      color = "inherit",
-      justify,
-      id = `UploadFichier${getRandomInt()}`,
       onChange,
     },
     ref
   ) => {
 
     const handleChange = e => {
-      onChange(e.target.files[0]);
-    };
-
-    const handleAccessibilite = e => {
-      if (e.key === " " || e.key === "Enter") {
-        const element = document.getElementById(id);
-        element.click();
-      }
+      onChange(convertImage(e.target.files[0]));
     };
 
     return (
-      <Grid
-        container
-        sx={{
-          ...sx,
-          display: "",
-        }}
-        alignItems="center"
-        justifyContent={justify}
-      >
-        <Grid component="label" htmlFor={id}>
-          <Grid
+          <button
             component="input"
-            ref={ref}
             accept=".pdf"
-            style={{ display: "none" }}
-            id={id}
             type="file"
-            onChange={handleChange}
-          />
-          <Button
-            variant="contained"
-            component="span"
-            color={color}
-            sx={{
-              marginBottom: "10px",
-              marginTop: "10px",
-            }}
-            onKeyDown={handleAccessibilite}
+            onClick={handleChange}
           >
           Charger un fichier PDF
-          </Button>
-        </Grid>
-      </Grid>
+          </button>
     );
   }
 );
