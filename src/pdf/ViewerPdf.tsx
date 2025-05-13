665bfe8151bd0c5e5d05c73016baf22ea627e730
Lucas Macaux <lucas0203@orange.fr>
2025-05-13 21:53:37 +0200

    ⚡️ Remplacer UploadPdf par un composant natif HTML

--- src/pdf/ViewerPdf.tsx
+++ src/pdf/ViewerPdf.tsx
@@ -1,48 +1,25 @@
 import React, { ReactElement, useState } from 'react';
 import pdf2html from "pdf2html";
-import { Button, Grid } from "@mui/material";
 
 const ViewerPdf = function (): ReactElement {
 
-	const [fileName, setFileName] = useState("");
 	const [htmlPdf, setHtmlPdf] = useState(null);
 	const deleteFile = () => {
-		setFileName("");
 		setHtmlPdf(null);
-	}	
-    const displayPdf = () => {
+	}
+	const displayPdf = () => {
 		var pdf = (document.getElementById("pdf") as HTMLInputElement);
 		if (pdf?.files) {
 			var file = pdf.files[0];
-			setFileName(file.name);
 			// setHtmlPdf(pdf2html.html(file));
 		}
 	};
 
 	return <section id="viewerPdf">
-		<div>
-			<Grid component="label">
-				<Grid
-				component="input"
-				accept=".pdf"
-				style={{ display: "none" }}
-				type="file"
-				onChange={displayPdf}
-				/>
-				<Button
-				variant="contained"
-				component="span"
-				sx={{
-					marginBottom: "10px",
-					marginTop: "10px",
-				}}
-				>
-				Charger un fichier PDF
-				</Button>
-			</Grid>
-			<p id="fileName">{fileName}</p>
-			<button onClick={deleteFile}>Fermer le PDF chargé</button>
-		</div>
+		<form>
+			<input type="file" id="myFile" name="filename" />
+			<input type="reset" value="Fermer le PDF chargé" />
+		</form>
 		<div>
 			{htmlPdf}
 		</div>
