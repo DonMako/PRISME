18bc9c0c72e8eb01a8b5f2a156234225dae13db4
Lucas Macaux <lucas0203@orange.fr>
2025-07-04 00:28:47 +0200

    🐛 Réussir l'import du PDF

--- src/pdf/ViewerPdf.tsx
+++ src/pdf/ViewerPdf.tsx
@@ -2,21 +2,35 @@ import React, { ReactElement, useState } from 'react';
 
 const ViewerPdf = function (): ReactElement {
 
-	const [htmlPdf, setHtmlPdf] = useState(null);
-	const displayPdf = () => {
-		const file = myFile.files;
-		if (file) {
-			htmlPdf = file;
+	const [pdf, setPdf] = useState("");
+
+	function handleChange() {
+		const element = document.getElementById("myFile");
+		if (element != null) {
+			const files = (element as HTMLInputElement).files;
+			if (files != null) {
+				setPdf(files[0].name);
+				const reader = new FileReader();
+				reader.addEventListener(
+					"load",
+					() => {
+						document.querySelector("embed")!.src = (reader.result! as string);
+					},
+					false,
+				);
+				if (files[0]) {
+					reader.readAsDataURL(files[0]);
+				}
+			}
 		}
 	};
 
 	return <section id="viewerPdf">
 		<form>
-			<input type="file" id="myFile" name="filename" accept=".pdf" />
+			<input type="file" id="myFile" name="filename" accept=".pdf" onChange={handleChange} />
 			<input type="reset" value="Fermer le PDF chargé" />
-			<pdf onClick={displayPdf}>Afficher le PDF</button>
 		</form>
-		{htmlPdf}
+		{pdf && <embed src="" id="myPdf" type="application/pdf" width="100%" height="100%" />}
 	</section>
 }
 
